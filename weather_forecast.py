083f976832ed0e2c3fb792c42a5e3fc868003cf7
zmdlw <giraffe0582@gmail.com>
2022-05-29 14:51:13 +0900

    beautiful + flask

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,10 +1,24 @@
+from optparse import OptionGroup
 from urllib import request
 from bs4 import BeautifulSoup
+from flask import Flask
 
-target = request.urlopen("http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108")
+app = Flask(__name__)
+@app.route("/")
 
-soup = BeautifulSoup(target, "html.parser")
+def hello():
+    target = request.uurlopen("http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108")
+    
+    soup = BeautifulSoup(target, "html.parser")
 
-for location in soup.select("location"):
-    print("도시: ", location.select_one("city").string)
-    print("날씨: ", location.select_one("wf").string)
+    output = ""
+    for location in soup.select("location"):
+        output += "<h3>{}</h3>".format(location.select_one("city").string)
+        output += "날씨: {}</br>".format(location.select_one("city").string)
+        output += "최저/최고 기온: {}/{}"\
+            .format(\
+                location.select_one("tmn").string,\
+                location.select_one("tmx").string\
+            )
+        output += "<hr/>"
+    return output
